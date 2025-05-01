e3a7d436a8672a08b2a3326e7dee98b8e889affd
naticabti <naticabti@gmail.com>
2025-05-01 19:11:49 +0300

    Config Updates

--- next.config.ts
+++ next.config.ts
@@ -4,15 +4,15 @@ const nextConfig: NextConfig = {
     dangerouslyAllowSVG: true,
     unoptimized: true,
   },
-  // async rewrites() {
-  //   return [
-  //     {
-  //       source: "/admin/tinacms",
-  //       destination: "/admin/index.html",
-  //     },
-  //     // Keep the original admin route for our custom admin panel
-  //   ];
-  // },
+  async rewrites() {
+    return [
+      {
+        source: "/admin/tinacms",
+        destination: "/admin/index.html",
+      },
+      // Keep the original admin route for our custom admin panel
+    ];
+  },
   output: "export",
 };
 
